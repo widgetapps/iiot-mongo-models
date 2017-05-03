ae1ffa42be13589e75cb399ac568c62ee09ac21f
Darryl Patterson <widgetapps@gmail.com>
2017-05-03 13:25:33 -0400

    Updated binary salt issue for node =>6

--- models/user.model.js
+++ models/user.model.js
@@ -122,7 +122,7 @@ UserSchema.pre('save', function(next) {
  */
 UserSchema.methods.hashPassword = function(password) {
 	if (this.salt && password) {
-		return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+		return crypto.pbkdf2Sync(password, new Buffer(this.salt, 'binary'), 10000, 64).toString('base64');
 	} else {
 		return password;
 	}
