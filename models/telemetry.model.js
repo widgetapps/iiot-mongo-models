e1203dcd2245e8a917f85a837505c498f4b77076
Darryl Patterson <widgetapps@gmail.com>
2018-09-27 16:30:21 -0400

    Added samples to the telemetry values.

--- models/telemetry.model.js
+++ models/telemetry.model.js
@@ -69,6 +69,9 @@ var TelemetrySchema = new Schema({
             },
             current: {
                 type: Number
+            },
+            samples: {
+                type: Number
             }
         }
     }
