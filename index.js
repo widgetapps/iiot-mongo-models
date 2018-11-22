a054e98ebf438eb469e347758c5628a300fee13f
Darryl Patterson <widgetapps@gmail.com>
2018-11-22 16:18:57 -0500

    Forgot to export the new models.

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 
 var dumydata  = require('./dumydata');
 
-exports.Asset     = require('./models/asset.model');
-exports.Alert     = require('./models/alert.model');
-exports.Client    = require('./models/client.model');
-exports.Device    = require('./models/device.model');
-exports.Location  = require('./models/location.model');
-exports.Message   = require('./models/message.model');
-exports.Sensor    = require('./models/sensor.model');
-exports.Tag       = require('./models/tag.model');
-exports.Telemetry = require('./models/telemetry.model');
-exports.User      = require('./models/user.model');
-exports.Mqtt      = require('./models/mqtt.model');
+exports.Asset          = require('./models/asset.model');
+exports.Alert          = require('./models/alert.model');
+exports.Client         = require('./models/client.model');
+exports.Device         = require('./models/device.model');
+exports.Event          = require('./models/event.model');
+exports.EventTelemetry = require('./models/eventtelemetry.model');
+exports.Location       = require('./models/location.model');
+exports.Message        = require('./models/message.model');
+exports.Sensor         = require('./models/sensor.model');
+exports.Tag            = require('./models/tag.model');
+exports.Telemetry      = require('./models/telemetry.model');
+exports.User           = require('./models/user.model');
+exports.Mqtt           = require('./models/mqtt.model');
 
 exports.populateDumyData = dumydata.populate;
 
